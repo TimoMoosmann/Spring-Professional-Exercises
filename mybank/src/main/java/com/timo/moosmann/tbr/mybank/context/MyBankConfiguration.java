13fb75e15bea44cd2c542a65e6d3d6eb82ae005a
Timo <timo-moosmann@web.de>
2024-07-16 16:23:47 +0200

    Deleted unused import

--- mybank/src/main/java/com/timo/moosmann/tbr/mybank/context/MyBankConfiguration.java
+++ mybank/src/main/java/com/timo/moosmann/tbr/mybank/context/MyBankConfiguration.java
@@ -1,7 +1,6 @@
 package com.timo.moosmann.tbr.mybank.context;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.timo.moosmann.tbr.mybank.ApplicationLauncher;
 import org.springframework.context.annotation.Bean;
