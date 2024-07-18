af6d2ce4cad0fb858fd6e39eaabb3a7bb2ce3734
Timo <timo-moosmann@web.de>
2024-07-18 17:25:12 +0200

    Removed obsolete TestController.java and obsolete objectMapper Bean

--- mybank/src/main/java/com/timo/moosmann/tbr/mybank/context/ApplicationConfiguration.java
+++ mybank/src/main/java/com/timo/moosmann/tbr/mybank/context/ApplicationConfiguration.java
@@ -1,9 +1,6 @@
 package com.timo.moosmann.tbr.mybank.context;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.timo.moosmann.tbr.mybank.ApplicationLauncher;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
@@ -15,8 +12,4 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @EnableWebMvc
 public class ApplicationConfiguration {
 
-    @Bean
-    public ObjectMapper objectMapper() {
-        return new ObjectMapper().registerModule(new JavaTimeModule());
-    }
 }
