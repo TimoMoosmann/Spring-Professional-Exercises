5c1954f385f5cb84d13ec694b0a2e0d65a4d5159
Timo <timo-moosmann@web.de>
2024-07-20 11:32:19 +0200

    Added missing Response Status

--- mybank/src/main/java/com/timo/moosmann/tbr/mybank/web/GlobalControllerErrorHandler.java
+++ mybank/src/main/java/com/timo/moosmann/tbr/mybank/web/GlobalControllerErrorHandler.java
@@ -1,9 +1,11 @@
 package com.timo.moosmann.tbr.mybank.web;
 
 import com.timo.moosmann.tbr.mybank.model.ValidationError;
+import org.springframework.http.HttpStatus;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.List;
@@ -12,6 +14,7 @@ import java.util.List;
 public class GlobalControllerErrorHandler {
 
     @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ValidationError handle(MethodArgumentNotValidException methodArgumentNotValidException) {
         List<String> invalidFields = methodArgumentNotValidException.getFieldErrors().stream().map(
                 FieldError::getField
