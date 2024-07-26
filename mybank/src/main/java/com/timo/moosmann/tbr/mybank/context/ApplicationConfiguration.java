1401687209187965b145126b447d71d063c2b9f0
Timo <timo-moosmann@web.de>
2024-07-26 15:59:05 +0200

    - Added dataSource for h2 database

--- mybank/src/main/java/com/timo/moosmann/tbr/mybank/context/ApplicationConfiguration.java
+++ mybank/src/main/java/com/timo/moosmann/tbr/mybank/context/ApplicationConfiguration.java
@@ -1,6 +1,7 @@
 package com.timo.moosmann.tbr.mybank.context;
 
 import com.timo.moosmann.tbr.mybank.ApplicationLauncher;
+import org.h2.jdbcx.JdbcDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -11,11 +12,22 @@ import org.thymeleaf.spring6.templateresolver.SpringResourceTemplateResolver;
 import org.thymeleaf.spring6.view.ThymeleafViewResolver;
 import org.thymeleaf.templatemode.TemplateMode;
 
+import javax.sql.DataSource;
+
 @Configuration
 @ComponentScan(basePackageClasses = ApplicationLauncher.class)
 @PropertySource("classpath:application.properties")
 @EnableWebMvc
 public class ApplicationConfiguration {
+
+    @Bean
+    public DataSource dataSource() {
+        JdbcDataSource jdbcDataSource = new JdbcDataSource();
+        jdbcDataSource.setURL("jdbc:h2:~/db;INIT=RUNSCRIPT FROM 'classpath:schema.sql'");
+        jdbcDataSource.setUser("sa");
+        jdbcDataSource.setPassword("sa");
+        return jdbcDataSource;
+    }
 
     @Bean
     public SpringResourceTemplateResolver templateResolver(){
